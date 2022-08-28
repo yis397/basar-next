e5554f8c13822ef4b47ec7b6ab597710b07a5803
Jesus Morales <105616549+yis397@users.noreply.github.com>
2022-08-27 19:35:49 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,11 +6,9 @@ const nextConfig = {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
-<<<<<<< HEAD
-  }}
-=======
+
   }
 }
->>>>>>> cf9424f7212e91d53a420394c6f0b32e697377b0
+
 
 module.exports = nextConfig
